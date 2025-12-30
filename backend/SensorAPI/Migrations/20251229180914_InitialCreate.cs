41a9afa7beeebff235edbae60e0039c80ee6e0b1
thuminhngo <thunmt1209@gmail.com>
2025-12-30 23:57:27 +0700

    add senorAPI and connect database

---
+++ backend/SensorAPI/Migrations/20251229180914_InitialCreate.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace SensorApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Devices",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    type = table.Column<string>(type: "text", nullable: false),
+                    is_online = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Devices", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "sensor_data_table",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    device_id = table.Column<int>(type: "integer", nullable: false),
+                    type = table.Column<string>(type: "text", nullable: false),
+                    value = table.Column<double>(type: "double precision", nullable: false),
+                    received_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Deviceid = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_sensor_data_table", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_sensor_data_table_Devices_Deviceid",
+                        column: x => x.Deviceid,
+                        principalTable: "Devices",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_sensor_data_table_Deviceid",
+                table: "sensor_data_table",
+                column: "Deviceid");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "sensor_data_table");
+
+            migrationBuilder.DropTable(
+                name: "Devices");
+        }
+    }
+}
---
+++ backend/SensorAPI/Models/Device.cs
@@ -0,0 +1,10 @@
+﻿namespace SensorApi.Models
+{
+    public class Device
+    {
+        public int id { get; set; }
+        public string name { get; set; } = ""; // "Đèn phòng khách", "Quạt", "Cảm biến khói"
+        public string type { get; set; } = ""; // "Relay", "Sensor"
+        public bool is_online { get; set; } = false;
+    }
+}
