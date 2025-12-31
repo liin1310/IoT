50f3ab1c30446e45e4b997b89fc1db72ea8ecc6b
Phạm Thùy Linh <linh.ptt225356@sis.hust.edu.vn>
2026-01-01 00:27:34 +0700

    phần cứng
    Test gửi dữ liệu gas, temp, humid; nhận lệnh fan, light ok
    Chưa test được cửa + tắt cảnh báo

--- sketch_nov20a.ino
+++ sketch_nov20a.ino
@@ -1,136 +1,201 @@
-#include <Adafruit_Sensor.h> 
-#include <DHT.h>
-#include <DHT_U.h>
-#include <WiFi.h>
-#include <WiFiClientSecure.h>
+ #include <WiFi.h>
 #include <PubSubClient.h>
+#include <WiFiClientSecure.h>
+#include <DHT.h>
 
-// SET WIFI
-const char* ssid = "4D"; //Tên wifi kết nối(SỬA KHI DOWNLOAD VỀ)
-const char* password = "777788884"; //Mật khẩu wifi
-// THÔNG TIN HIVEMQ CLOUD
-const char* mqtt_server   = "2855c0cbdaa243e19e19b5b00d6dcc25.s1.eu.hivemq.cloud";  // URL
-const char* mqtt_username = "test-data";           // Username
-const char* mqtt_password = "Linh2612@";
+// Cấu hình Wifi và MQTT
+const char* ssid = "Liin";
+const char* password = "25102004";
+const char* mqtt_server = "954aa7485e03459f8a6ba21673896477.s1.eu.hivemq.cloud";
+const int mqtt_port = 8883;
+const char* mqtt_user = "user1";
+const char* mqtt_pass = "Linh13102004";
 
-#define MQTT_PORT 8883 // Cổng server mqtt
-#define DHT_PIN 13 // Chân DATA của DHT11 (D13) (THAY ĐỔI NẾU CẮM SANG CHÂN KHÁC)
-#define DHT_TYPE DHT11
-#define LED_PIN 15 //Chân data của LED (chân dài) (THAY ĐỔI NẾU CẮM SANG CHÂN KHÁC)
+//Khai báo chân 
+#define DHT_TYPE 11
+#define MQ2_PIN     32   
+#define LED_ALARM_PIN     2
+#define LED_LIGHT_PIN     4
+#define BUZZER_PIN  15
+#define DHT_PIN     13
 
-#define TOPIC_TEMP "iot/sensor/temperature"
-#define TOPIC_HUM "iot/sensor/humidity"
-#define TOPIC_LED  "iot/sensor/led/control"
+#define RELAY1_PIN  25 //Giả lập QUẠT
+#define RELAY2_PIN  26 //Giả lập Cửa
+#define RELAY3_PIN  27
+#define RELAY4_PIN  14
 
-DHT_Unified dht(DHT_PIN, DHT_TYPE);
+//TOPIC
+//1. topic gửi dữ liệu (publish)
+#define TOPIC_DATA_TEMP   "home/data/temp"
+#define TOPIC_DATA_HUMID  "home/data/humid"
+#define TOPIC_DATA_GAS    "home/data/gas"
+#define TOPIC_STATUS_FIRE "home/status/fire"
+
+//2. topic nhận lệnh điều khiển (subscribe)
+#define TOPIC_CMD_FAN   "home/cmd/fan"   // Lệnh: ON/OFF
+#define TOPIC_CMD_DOOR    "home/cmd/door"    // Lệnh: OPEN/CLOSE
+#define TOPIC_CMD_ALARM  "home/cmd/alarm"  // Lệnh tắt còi 
+#define TOPIC_CMD_LIGHT  "home/cmd/light"
+
+DHT dht(DHT_PIN, DHT_TYPE);
 WiFiClientSecure espClient;
 PubSubClient client(espClient);
 
-unsigned long lastSend = 0;
-const long interval = 5000;   // Gửi dữ liệu mỗi 5 giây
+int gasThreshold = 2000;
+float tempThreshold = 45.0; 
+unsigned long lastMsg = 0;
+bool isAlarmActive = false;   // Trạng thái đang báo động
+bool isAlarmSilenced = false; // Trạng thái đã bấm nút tắt còi
 
+// HÀM XỬ LÝ LỆNH TỪ SERVER
+void callback(char* topic, byte* payload, unsigned int length) {
+  String message = "";
+  for (int i = 0; i < length; i++) message += (char)payload[i];
+  String strTopic = String(topic);
 
-void setup() {
-  Serial.begin(115200); 
-  pinMode(LED_PIN, OUTPUT); //
-  digitalWrite(LED_PIN, LOW); //Set đèn sáng khi khởi động
-  dht.begin(); //Bắt đầu khởi tạo
+  Serial.print("Nhận lệnh MQTT ["); Serial.print(strTopic);
+  Serial.print("]: "); Serial.println(message);
 
-  connectWiFi();
-  
-  // Cấu hình TLS cho HiveMQ Cloud
-  espClient.setInsecure();       // Bỏ qua xác thực cert
-  client.setServer(mqtt_server, MQTT_PORT);
-  // client.setCallback(callback);
-  
-  Serial.println("=== Hệ thống sẵn sàng ===");
-}
-   
-
-void loop() {
-  if(!client.connected()){ //Kết nối lại nếu kết nối thất bại
-    reconnectMQTT();
-  }
-  client.loop();
-
-  unsigned long now = millis();
-  if (now - lastSend > interval) {
-    lastSend = now;
-    readAndSendDHT();    // Đọc DHT11 và gửi lên HiveMQ
-    client.setCallback(callback);
-  }
-}
-
-// =============    CÁC HÀM HỖ TRỢ  ============================
-void connectWiFi() {
-  Serial.print("Đang kết nối WiFi ");
-  Serial.print(ssid);
-  WiFi.begin(ssid, password);
-  
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+  // 1. Điều khiển QUẠT (Relay 1)
+  if (strTopic == TOPIC_CMD_FAN) {
+    if (message == "ON") digitalWrite(RELAY1_PIN, LOW); // low = bật
+    else if (message == "OFF") digitalWrite(RELAY1_PIN, HIGH);
   }
   
-  Serial.println("\nWiFi đã kết nối!");
-  Serial.print("IP: ");
-  Serial.println(WiFi.localIP());
+  // 2. Điều khiển CỬA (Relay 2)
+  else if (strTopic == TOPIC_CMD_DOOR) {
+    if (message == "OPEN") digitalWrite(RELAY2_PIN, LOW); 
+    else if (message == "CLOSE") digitalWrite(RELAY2_PIN, HIGH);
+  }
+
+  // 3. Tắt báo động từ Web
+  else if (strTopic == TOPIC_CMD_ALARM && message == "OFF") {
+    stopAlarm();
+  }
+
+  // 4. Điều khiển đèn
+  else if (strTopic == TOPIC_CMD_LIGHT) {
+    if (message == "ON") {
+        digitalWrite(LED_LIGHT_PIN, HIGH); // Bật LED
+        Serial.println("-> DA BAT DEN (LED 4)");
+    } 
+    else if (message == "OFF") {
+        digitalWrite(LED_LIGHT_PIN, LOW);  // Tắt LED
+        Serial.println("-> DA TAT DEN (LED 4)");
+    }
+  }
 }
 
-void reconnectMQTT() {
+// HÀM KẾT NỐI
+void reconnect() {
   while (!client.connected()) {
-    Serial.print("Đang kết nối HiveMQ Cloud...");
-    if (client.connect("ESP32_Client", mqtt_username, mqtt_password)) {
-      Serial.println(" Đã kết nối MQTT!");
-      client.subscribe(TOPIC_LED);
-      // client.publish(TOPIC_LED,""); 
+    Serial.print("Connecting MQTT...");
+    String clientId = "ESP32Client-" + String(random(0xffff), HEX);
+    if (client.connect(clientId.c_str(), mqtt_user, mqtt_pass)) {
+      Serial.println("SUCCESS!");
+      client.subscribe(TOPIC_CMD_FAN);
+      client.subscribe(TOPIC_CMD_LIGHT);
+      client.subscribe(TOPIC_CMD_DOOR);
+      client.subscribe(TOPIC_CMD_ALARM);
     } else {
-      Serial.print(" Lỗi, rc=");
-      Serial.print(client.state());
-      Serial.println(" Thử lại sau 5 giây...");
       delay(5000);
     }
   }
 }
-void callback(char* topic, byte* payload, unsigned int length) {
-  String msg = "";
-  for (int i = 0; i < length; i++) msg += (char)payload[i];
 
-  if (String(topic) == TOPIC_LED) {
-    int value;
-    if (msg == "ON") value=100;
-    else if (msg == "OFF") value=0;
-    else value = msg.toInt();
-    setBrightnessPercent(value); //Chỉnh độ sáng theo %
+// HÀM BẬT BÁO ĐỘNG
+void triggerAlarm() {
+  if (!isAlarmActive && !isAlarmSilenced) {
+    isAlarmActive = true;
+    digitalWrite(BUZZER_PIN, HIGH); // Còi kêu
+    digitalWrite(LED_ALARM_PIN, HIGH);    // Đèn trên mạch sáng
+    client.publish(TOPIC_STATUS_FIRE, "WARNING");
+    Serial.println("-> CANH BAO CHAY KICH HOAT!");
   }
 }
-void readAndSendDHT() {
-  sensors_event_t event;
-  float temp = -999;
-  float hum  = -999;
 
-  dht.temperature().getEvent(&event);
-  if (!isnan(event.temperature)) {
-    temp = event.temperature;
-    client.publish(TOPIC_TEMP, String(temp, 1).c_str());
-    Serial.printf("{ Nhiệt độ: %.1f°C ,", temp);
-  }
-
-  dht.humidity().getEvent(&event);
-  if (!isnan(event.relative_humidity)) {
-    hum = event.relative_humidity;
-    client.publish(TOPIC_HUM, String(hum, 1).c_str());
-    Serial.printf(" Độ ẩm: %.1f%% }\n", hum);
-  }
-
-  if (temp == -999 || hum == -999) {
-    Serial.println("Lỗi đọc cảm biến DHT11!");
+// --- HÀM TẮT BÁO ĐỘNG ---
+void stopAlarm() {
+  if (isAlarmActive) {
+    isAlarmActive = false;
+    isAlarmSilenced = true; // Ghi nhớ là đã tắt thủ công
+    digitalWrite(BUZZER_PIN, LOW); // Tắt còi
+    digitalWrite(LED_ALARM_PIN, LOW);
+    client.publish(TOPIC_STATUS_FIRE, "SAFE");
+    Serial.println("-> DA TAT COI TU WEB.");
   }
 }
-// Hàm chỉnh độ sáng
-void setBrightnessPercent(int percent) {
-  if (percent < 0) percent = 0;
-  if (percent > 100) percent = 100;
-  int pwmValue = map(percent, 0, 100, 0, 255);
-  analogWrite(LED_PIN, pwmValue);
+
+void setup() {
+  Serial.begin(115200);
+  dht.begin();
+
+  //Cấu hình chân
+  pinMode(LED_ALARM_PIN, OUTPUT);
+  pinMode(LED_LIGHT_PIN, OUTPUT);
+  pinMode(BUZZER_PIN, OUTPUT);
+  pinMode(RELAY1_PIN, OUTPUT);
+  pinMode(RELAY2_PIN, OUTPUT);
+  pinMode(RELAY3_PIN, OUTPUT);
+  pinMode(RELAY4_PIN, OUTPUT);
+
+  //Trạng thái ban đầu
+  digitalWrite(RELAY1_PIN, HIGH);
+  digitalWrite(RELAY2_PIN, HIGH);
+  digitalWrite(RELAY3_PIN, HIGH);
+  digitalWrite(RELAY4_PIN, HIGH);
+  digitalWrite(LED_ALARM_PIN, LOW);
+  digitalWrite(LED_LIGHT_PIN, LOW);
+  digitalWrite(BUZZER_PIN, LOW);
+
+  // Kết nối Wifi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500); Serial.print(".");
+  }
+  Serial.println("\nWifi Connected");
+
+  espClient.setInsecure();
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+}
+void loop() {
+  if (!client.connected()) reconnect();
+  client.loop();
+  unsigned long now = millis();
+  if (now - lastMsg > 2000){//Cách 2 giây
+    lastMsg = now;
+    int gasVal = analogRead(MQ2_PIN);
+    float temperature = dht.readTemperature(); 
+    float humidity = dht.readHumidity(); 
+    if (isnan(temperature) || isnan(humidity)) {
+      Serial.println("không thể đọc dữ  liệu");
+      delay(1000);
+      return;
+    }
+
+    // Gửi lên MQTT
+    char strVal[10];
+    itoa(gasVal, strVal, 10);
+    client.publish(TOPIC_DATA_GAS, strVal);
+
+    dtostrf(temperature, 1, 2, strVal);
+    client.publish(TOPIC_DATA_TEMP, strVal);
+    
+    dtostrf(humidity, 1, 2, strVal);
+    client.publish(TOPIC_DATA_HUMID, strVal);
+
+    Serial.printf("Gas: %d | Temp: %.1f | Hum: %.1f\n", gasVal, temperature, humidity);
+
+    // 3. Logic Báo động tại chỗ
+    if (gasVal > gasThreshold || temperature > tempThreshold) {
+      triggerAlarm();
+    } else {
+      if (isAlarmSilenced && gasVal < (gasThreshold - 100)) {
+        isAlarmSilenced = false;
+        isAlarmActive = false;
+        Serial.println("->SAFE (reset).");
+      }
+    }
+  }
 }
