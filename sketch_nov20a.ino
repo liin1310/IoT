4c978de2ba20eef9cde7772d07942c4640f2ebbb
Phạm Thùy Linh <linh.ptt225356@sis.hust.edu.vn>
2026-01-06 01:55:08 +0700

    Add servo control and update MQTT topics

--- sketch_nov20a.ino
+++ sketch_nov20a.ino
@@ -2,6 +2,7 @@
 #include <WiFiClientSecure.h>
 #include <DHT.h>
 #include <WiFiManager.h> 
+#include <ESP32Servo.h>
 
 // Cấu hình MQTT
 const char* mqtt_server = "954aa7485e03459f8a6ba21673896477.s1.eu.hivemq.cloud";
@@ -18,24 +19,30 @@ const char* mqtt_pass = "Linh13102004";
 #define DHT_PIN     13
 
 #define RELAY1_PIN  25 //Giả lập QUẠT
-#define RELAY2_PIN  26 //Giả lập Cửa
-#define RELAY3_PIN  27
-#define RELAY4_PIN  14
+#define SERVO_PIN  26 
+#define SW_LIGHT_PIN  27
+#define SW_ALARM_PIN  14
 
 //TOPIC
 //1. topic gửi dữ liệu (publish)
 #define TOPIC_DATA_TEMP   "home/data/temp"
 #define TOPIC_DATA_HUMID  "home/data/humid"
 #define TOPIC_DATA_GAS    "home/data/gas"
-#define TOPIC_STATUS_FIRE "home/status/fire"
 
-//2. topic nhận lệnh điều khiển (subscribe)
+//2. topic gửi trạng thái (publish)
+#define TOPIC_STATUS_FAN    "home/status/fan"
+#define TOPIC_STATUS_DOOR   "home/status/door"
+#define TOPIC_STATUS_FIRE   "home/status/fire"
+#define TOPIC_STATUS_LIGHT  "home/status/light"
+
+//3. topic nhận lệnh điều khiển (subscribe)
 #define TOPIC_CMD_FAN   "home/cmd/fan"   // Lệnh: ON/OFF
 #define TOPIC_CMD_DOOR    "home/cmd/door"    // Lệnh: OPEN/CLOSE
 #define TOPIC_CMD_ALARM  "home/cmd/alarm"  // Lệnh tắt còi 
 #define TOPIC_CMD_LIGHT  "home/cmd/light"
 
 DHT dht(DHT_PIN, DHT_TYPE);
+Servo doorServo;
 WiFiClientSecure espClient;
 PubSubClient client(espClient);
 
@@ -44,6 +51,8 @@ float tempThreshold = 45.0;
 unsigned long lastMsg = 0;
 bool isAlarmActive = false;   // Trạng thái đang báo động
 bool isAlarmSilenced = false; // Trạng thái đã bấm nút tắt còi
+int lastLightSwState = HIGH; //Trạng thái ban đầu của công tắc
+int lastStopSwState = HIGH;
 
 // HÀM XỬ LÝ LỆNH TỪ SERVER
 void callback(char* topic, byte* payload, unsigned int length) {
@@ -56,14 +65,26 @@ void callback(char* topic, byte* payload, unsigned int length) {
 
   // 1. Điều khiển QUẠT (Relay 1)
   if (strTopic == TOPIC_CMD_FAN) {
-    if (message == "ON") digitalWrite(RELAY1_PIN, LOW); // low = bật
-    else if (message == "OFF") digitalWrite(RELAY1_PIN, HIGH);
+    if (message == "ON") {
+      digitalWrite(RELAY1_PIN, LOW); // low = bật
+      client.publish(TOPIC_STATUS_FAN, "ON");
+    }
+    else if (message == "OFF") {
+      digitalWrite(RELAY1_PIN, HIGH);
+      client.publish(TOPIC_STATUS_FAN, "OFF");
+    }
   }
   
-  // 2. Điều khiển CỬA (Relay 2)
+  // 2. Điều khiển CỬA 
   else if (strTopic == TOPIC_CMD_DOOR) {
-    if (message == "OPEN") digitalWrite(RELAY2_PIN, LOW); 
-    else if (message == "CLOSE") digitalWrite(RELAY2_PIN, HIGH);
+    if (message == "ON") {
+      doorServo.write(90); 
+      client.publish(TOPIC_STATUS_DOOR, "ON");
+    }
+    else if (message == "OFF") {
+      doorServo.write(0);
+      client.publish(TOPIC_STATUS_DOOR, "OFF");
+    }
   }
 
   // 3. Tắt báo động từ Web
@@ -75,11 +96,11 @@ void callback(char* topic, byte* payload, unsigned int length) {
   else if (strTopic == TOPIC_CMD_LIGHT) {
     if (message == "ON") {
         digitalWrite(LED_LIGHT_PIN, HIGH); // Bật LED
-        Serial.println("-> DA BAT DEN (LED 4)");
+        client.publish(TOPIC_STATUS_LIGHT, "ON");
     } 
     else if (message == "OFF") {
         digitalWrite(LED_LIGHT_PIN, LOW);  // Tắt LED
-        Serial.println("-> DA TAT DEN (LED 4)");
+        client.publish(TOPIC_STATUS_LIGHT, "OFF");
     }
   }
 }
@@ -106,13 +127,17 @@ void triggerAlarm() {
   if (!isAlarmActive && !isAlarmSilenced) {
     isAlarmActive = true;
     digitalWrite(BUZZER_PIN, HIGH); // Còi kêu
-    digitalWrite(LED_ALARM_PIN, HIGH);    // Đèn trên mạch sáng
+    digitalWrite(LED_ALARM_PIN, HIGH);    // Đèn sáng
+
+    doorServo.write(90); //Cửa tự động mở
+    client.publish(TOPIC_STATUS_DOOR, "ON");
+
     client.publish(TOPIC_STATUS_FIRE, "WARNING");
-    Serial.println("-> CANH BAO CHAY KICH HOAT!");
+    Serial.println("-> CẢNH BÁO CHÁY ĐƯỢC KÍCH HOẠT!");
   }
 }
 
-// --- HÀM TẮT BÁO ĐỘNG ---
+// HÀM TẮT BÁO ĐỘNG
 void stopAlarm() {
   if (isAlarmActive) {
     isAlarmActive = false;
@@ -120,31 +145,69 @@ void stopAlarm() {
     digitalWrite(BUZZER_PIN, LOW); // Tắt còi
     digitalWrite(LED_ALARM_PIN, LOW);
     client.publish(TOPIC_STATUS_FIRE, "SAFE");
-    Serial.println("-> DA TAT COI TU WEB.");
+    Serial.println("-> ĐÃ TẮT CẢNH BÁO CHÁY TỪ WEB.");
+  }
+}
+
+// HÀM KIỂU TRA CÔNG TẮC 
+void checkSwitches() {
+  // CÔNG TẮC ĐÈN  
+  int currentLightState = digitalRead(SW_LIGHT_PIN);
+
+  if (currentLightState != lastLightSwState) { //Có người gạt công tắc??
+    delay(30); //Chống nhiễu
+    if (digitalRead(SW_LIGHT_PIN) == currentLightState) {
+      
+      bool ledState = digitalRead(LED_LIGHT_PIN);
+      digitalWrite(LED_LIGHT_PIN, !ledState); 
+
+      if (!ledState) { 
+        client.publish(TOPIC_STATUS_LIGHT, "ON");
+        Serial.println("-> Switch: Đã bật đèn");
+      } else {
+        client.publish(TOPIC_STATUS_LIGHT, "OFF");
+        Serial.println("-> Switch: Đã tắt đèn");
+      }
+      lastLightSwState = currentLightState;
+    }
+  }
+
+  // CÔNG TẮC TẮT BÁO ĐỘNG
+  int currentStopState = digitalRead(SW_ALARM_PIN);
+
+  if (currentStopState != lastStopSwState) {
+     delay(30);
+     if (digitalRead(SW_ALARM_PIN) == currentStopState) {
+        stopAlarm(); 
+        Serial.println("-> Switch: Đã tắt báo động.");
+        lastStopSwState = currentStopState;
+     }
   }
 }
 
 void setup() {
   Serial.begin(115200);
   dht.begin();
+  doorServo.attach(SERVO_PIN);
+  doorServo.write(0); //Mặc định đóng cửa
 
   //Cấu hình chân
   pinMode(LED_ALARM_PIN, OUTPUT);
   pinMode(LED_LIGHT_PIN, OUTPUT);
   pinMode(BUZZER_PIN, OUTPUT);
   pinMode(RELAY1_PIN, OUTPUT);
-  pinMode(RELAY2_PIN, OUTPUT);
-  pinMode(RELAY3_PIN, OUTPUT);
-  pinMode(RELAY4_PIN, OUTPUT);
+  pinMode(SW_LIGHT_PIN, INPUT_PULLUP);
+  pinMode(SW_ALARM_PIN, INPUT_PULLUP);
 
   //Trạng thái ban đầu
   digitalWrite(RELAY1_PIN, HIGH);
-  digitalWrite(RELAY2_PIN, HIGH);
-  digitalWrite(RELAY3_PIN, HIGH);
-  digitalWrite(RELAY4_PIN, HIGH);
   digitalWrite(LED_ALARM_PIN, LOW);
   digitalWrite(LED_LIGHT_PIN, LOW);
   digitalWrite(BUZZER_PIN, LOW);
+
+  // Đọc trạng thái ban đầu
+  lastLightSwState = digitalRead(SW_LIGHT_PIN);
+  lastStopSwState = digitalRead(SW_ALARM_PIN);
 
   WiFiManager vm;
   vm.setConfigPortalTimeout(180); //Set timeout 3ph
@@ -163,6 +226,9 @@ void setup() {
 void loop() {
   if (!client.connected()) reconnect();
   client.loop();
+
+  checkSwitches();
+
   unsigned long now = millis();
   if (now - lastMsg > 2000){//Cách 2 giây
     lastMsg = now;
@@ -170,7 +236,7 @@ void loop() {
     float temperature = dht.readTemperature(); 
     float humidity = dht.readHumidity(); 
     if (isnan(temperature) || isnan(humidity)) {
-      Serial.println("không thể đọc dữ  liệu");
+      Serial.println("không thể đọc dữ liệu");
       delay(1000);
       return;
     }
@@ -195,7 +261,7 @@ void loop() {
       if (isAlarmSilenced && gasVal < (gasThreshold - 100)) {
         isAlarmSilenced = false;
         isAlarmActive = false;
-        Serial.println("->SAFE (reset).");
+        Serial.println("->AN TOÀN (reset).");
       }
     }
   }
