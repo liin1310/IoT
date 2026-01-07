b106cc0e047d654bca32c743d7b6f19964e65873
thuminhngo <thunmt1209@gmail.com>
2026-01-07 23:19:36 +0700

    fix check-fire

---
+++ backend/SensorAPI/Migrations/20260107161740_AddFireNotificationState.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace SensorApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFireNotificationState : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "fire_notification_state",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    last_fire_state = table.Column<bool>(type: "boolean", nullable: false),
+                    last_fire_notification_time = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_fire_notification_state", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "fire_notification_state");
+        }
+    }
+}
